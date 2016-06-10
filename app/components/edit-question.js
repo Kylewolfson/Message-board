915ce76d7a676b3847bcee24398caa12b123f040
Kyle Wolfson <wolfsonk@gmail.com>
2016-06-10 12:41:46 -0700

    finished question edit function

--- app/components/edit-question.js
+++ app/components/edit-question.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    editQuestion() {
+      var params = {
+        text: this.get('text'),
+        author: this.get('author'),
+        notes: this.get('notes')
+      };
+      var question = this.get('question');
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key, params[key]);
+        }
+      });
+      question.save();
+    }
+  }
 });
