1166025588d5ec18485300be92d62aa9ad28a4fb
Kyle Wolfson <wolfsonk@gmail.com>
2016-06-10 11:44:47 -0700

    add functionality for comment additions and displays. Found missing s on component display that was breaking everything. Also added new question form. Next step is adding question editing.

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    newAnswer() {
+      var params = {
+        text: this.get('text'),
+        author: this.get('author'),
+        question: this.get('question')
+      };
+    this.sendAction('newAnswer', params);
+    }
+  }
 });
--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    newQuestion() {
+      var params = {
+        text: this.get('text'),
+        author: this.get('author'),
+        notes: this.get('notes')
+      };
+    this.sendAction('newQuestion', params);
+    }
+  }
 });
