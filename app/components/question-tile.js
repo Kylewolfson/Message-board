f8a37b9a5a14d70ae76efb28d51615f50a020c43
Kyle Wolfson <wolfsonk@gmail.com>
2016-06-17 08:55:18 -0700

    added computed property for number of answers a question has

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  answerCount: Ember.computed('question.answers', function() {
+    return this.get('question.answers.length');
+  })
+
 });
